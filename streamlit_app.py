72c6acd899eb6538c1332ffa487b6bc0595578ae
andriusmaximus <90650278+andriusmaximus@users.noreply.github.com>
2023-09-29 14:51:29 -0400

    Replace the app with Diner App

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,43 +1,15 @@
 import streamlit
-import snowflake.connector
 import pandas
 
-streamlit.title('Zena\'s Amazing Athleisure Catalog')
+streamlit.title('My Parents New Healthy Diner')
 
-# connect to snowflake
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
-# run a snowflake query and put it all in a var called my_catalog
-my_cur.execute("select color_or_style from catalog_for_website")
-my_catalog = my_cur.fetchall()
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-# put the data into a dataframe
-df = pandas.DataFrame(my_catalog)
-
-# temp write the dataframe to the page so I Can see what I am working with
-# streamlit.write(df)
-
-# put the first column into a list
-color_list = df[0].values.tolist()
-# print(color_list)
-
-# Let's put a pick list here so they can pick the color
-option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
-
-# We'll build the image caption now, since we can
-product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
-
-# use the option selected to go back and get all the info from the database
-my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = '" + option + "';")
-df2 = my_cur.fetchone()
-
-streamlit.image(
-  df2[0],
-  width=400,
-  caption= product_caption
-  )
-
-streamlit.write('Price: ', df2[1])
-streamlit.write('Sizes Available: ',df2[2])
-streamlit.write(df2[3])
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
