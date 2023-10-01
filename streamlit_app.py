5567700cf212ed4ef35e5fd7577402161077addb
andriusmaximus <90650278+andriusmaximus@users.noreply.github.com>
2023-10-01 16:31:31 -0400

    Fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ except URLError as e:
 streamlit.header("The fruit list contains:")
 # snowflake function
 def get_fruit_load_list():
-  with my_cnx.cursor() as my cur:
+  with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from fruit_load_list")
     return my_cur.fetchall()
 
